dddac4be39762d7a488ba99b5ebe578f6f49372c
Sarah Heavner <sheavner@gmail.com>
2021-01-10 13:42:54 -0700

    updated chart function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -12,22 +12,25 @@ d3.json("../data/samples.json").then((data) => {
   });
 
   panelInfo("940");
+  chartInfo("940");
 
 
 
 //Create function that uses d3 to read json data and append metadata to panel
 function panelInfo(personID) {
   d3.json("../data/samples.json").then((data) => {
+    //store metadata into variable to filter
     var metadata = data.metadata;
+    //Filter data by personID
     var filterData = metadata.filter((firstObject) => firstObject.id == personID);
+    //Set first result as variable and log in console
     var firstResult = filterData[0];
     console.log(firstResult);
-      
-    //Use d3 to create reference to panel
+    //Use d3 to create reference to metadata panel
     var infoPanel = d3.select("#sample-metadata");
     //Clear any previous metadata
     infoPanel.html("");
-    //Use object.entries to select key/value for object
+    //Use object.entries to select key/value for initial result
     Object.entries(firstResult).forEach(([key, value]) => {
         //Append key/value to panel
       infoPanel.append("h6").text(`${key}:${value}`);
@@ -38,9 +41,14 @@ function panelInfo(personID) {
 //Create function that uses d3 to read json data and append graphs
 function chartInfo(personID) {
   d3.json("../data/samples.json").then((data) => {
-    var otu_ids = data.otu_ids;
-    var sample_values = data.sample_values;
-    var otu_labels = data.otu_labels;
+    var chartData = data.samples;
+    var filterChart = chartData.filter((firstSample) => firstSample.id == personID);
+    var firstChart = filterChart[0];
+    console.log(firstChart);
+
+    var otu_ids = firstChart.otu_ids;
+    var sample_values = firstChart.sample_values;
+    var otu_labels = firstChart.otu_labels;
 
     var barTrace = {
       x: sample_values,
