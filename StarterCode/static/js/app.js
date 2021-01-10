e309860afb2cf37d70dbe8c5a2dde25e97a90db2
Sarah Heavner <sheavner@gmail.com>
2021-01-10 08:11:38 -0700

    added names to dropdown menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,14 @@
-var data;
-
 // Fetch the JSON data and console log it
 d3.json("../data/samples.json").then(function(data) {
     console.log(data);
   });
-            
+
+
+// Use d3 to read json data and append names to dropdown menu
+d3.json("../data/samples.json").then((data) => {
+  data.names.forEach((person) => {
+  d3.select("#selDataset").append("option")
+                          .text(person)
+                          .property("value", person);
+      });
+  });
